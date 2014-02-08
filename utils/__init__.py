c59a6210f31e5bb972d756d8ff1f9fa4ce727668
Hugo Bastien <hugobast@gmail.com>
2014-02-07 21:49:50 -0500

    privatized lazy_prop

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,8 +2,8 @@ def lazy_property(fn):
     attr_name = '_lazy_' + fn.__name__
 
     @property
-    def _lazy_property(self):
+    def __lazy_property__(self):
         if not hasattr(self, attr_name):
             setattr(self, attr_name, fn(self))
         return getattr(self, attr_name)
-    return _lazy_property
+    return __lazy_property__
