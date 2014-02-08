3c3b4f695a1aa6a11b14e8f730bdadc8de2c6071
Hugo Bastien <hugobast@gmail.com>
2014-02-08 12:55:39 -0500

    trial and error figuring out what the interface to nose plugin and it's quirks

--- nosepride/plugin_base.py
+++ nosepride/plugin_base.py
@@ -1,12 +1,28 @@
+from os import devnull
 from nose.plugins import Plugin
 
 # Plugin interface methods
+# https://nose.readthedocs.org/en/latest/plugins/interface.html
+class NullStream(object):
+
+    def __init__(self, stream):
+        self.stream = stream
+
+    def __getattr__(self, name):
+        return getattr(self.stream, name)
+
+    def write(self, *args):
+        return
+
+    def writeln(self, *args):
+        return self.stream.writeln(*args)
+
 
 class PluginBase(Plugin):
 
     name = 'nosepride'
-    enabled = False
-    score = 100
+    enabled = True
+    score = 199
 
     def options(self, parser, env):
         parser.add_option(
@@ -17,24 +33,38 @@ class PluginBase(Plugin):
             help="enable colour output"
         )
 
+    def begin(self):
+        self.running_test = False
+
+    def beforeTest(self, test):
+        self.running_test = True
+
+    def afterTest(self, test):
+        if self.running_test:
+            self.addSkip()
+
     def configure(self, options, conf):
         if options.fabulous:
             self.enabled = True
 
-    def addFailure(self, test, err):
-        self.out(self.failure("f"))
-
-    def addError(self, test, err):
-        self.out(self.failure("e"))
-
-    def addSuccess(self, test):
-        self.out(self.pride("."))
-
-    def addSkip(self, test=None, err=None):
-        self.out(self.pride("*"))
+    def prepareTestResult(self, result):
+        result.stream = NullStream(result.stream)
 
     def setOutputStream(self, stream):
         self.stream = stream
 
+    def addFailure(self, test, err):
+        self.output(self.failure("f"))
+
+    def addError(self, test, err):
+        self.output(self.failure("e"))
+
+    def addSuccess(self, test):
+        self.output(self.pride("."))
+
+    def addSkip(self, test=None, err=None):
+        self.output(self.pride("*"))
+
     def output(self, string):
         self.stream.write(string)
+        self.running_test = False
