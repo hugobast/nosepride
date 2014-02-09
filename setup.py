fa142cec4931b6194bca44c1bba189f5b4556460
Hugo Bastien <hugobast@gmail.com>
2014-02-09 18:02:39 -0500

    preparing for version 0.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author="Hugo Bastien",
     author_email="hugobast@gmail.com",
     url="https://github.com/hugobast/nosepride.git",
-    version="0.0.1",
+    version="0.0.2",
     packages=[
         "nosepride",
         "nosepride/formatters",
@@ -21,6 +21,7 @@ setup(
     ],
     test_suite='tests',
     license="MIT License",
+    description="Fabulous colors for nosetests",
     long_description=open("README.txt").read(),
     entry_points = {
         'nose.plugins.0.10': [
