7bd9d5876a61faa3ec0791ff9d7a84f34203870a
Hugo Bastien <hugobast@gmail.com>
2014-02-18 22:34:22 -0500

    Updated version to 0.0.7

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author="Hugo Bastien",
     author_email="hugobast@gmail.com",
     url="https://github.com/hugobast/nosepride.git",
-    version="0.0.6",
+    version="0.0.7",
     packages=[
         "nosepride",
         "nosepride/formatters",
